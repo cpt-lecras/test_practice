ef6fd15631532964ca40a9cf7cd17a1dc14eb405
LeCras <55882565+cpt-lecras@users.noreply.github.com>
2025-01-21 20:19:16 +0300

    mycommit

---
+++ main.cs
@@ -0,0 +1,12 @@
+namespace hellow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hellow World");
+            Console.WriteLine("Hellow World2");
+            Console.ReadKey();
+        }
+    }
+}
